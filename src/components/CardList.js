0cac8e54ae798d2272d398d1ac1207cd51045b72
SNasser97 <snasserahmed@outlook.com>
2020-09-22 11:18:39 +0100

    format cardlist

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,17 +4,27 @@ import Card from "./Card";
 // {property} = props/property
 const CardList = ({ property }) => {
 	// const cardsArray = 
-	
 	return (
-			<div className="card-container">
-				{
-					property.map(house => {
-						return (
-							<Card key={ house["id"] } features={ house["house_features"] }  id={ house["id"]}  name={ house["house_address"] }  area={ house["house_area"] }  postcode={ house["house_postcode"] }  type={ house["house_type"] }  desc={ house["house_desc"] }  price={ house["house_price"] }  image={ house["imageData"] }/>
-						)
-					})
-				}
-			</div>
+		<div className="card-container">
+			{
+				property.map(house => {
+					return (
+						<Card 
+							key={ house["id"] } 
+							features={ house["house_features"] }  
+							id={ house["id"]}  
+							name={ house["house_address"] }  
+							area={ house["house_area"] }  
+							postcode={ house["house_postcode"] }  
+							type={ house["house_type"] }  
+							desc={ house["house_desc"] }  
+							price={ house["house_price"] }  
+							image={ house["imageData"] }
+						/>
+					)
+				})
+			}
+		</div>
 	);
 }
 
