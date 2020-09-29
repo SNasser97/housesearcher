4841e50441eeb5c77ebf0b905fe11b9ad6054119
SNasser97 <snasserahmed@outlook.com>
2020-09-29 11:56:01 +0100

    add react search icon + styling, use aria label for search text

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { VscSearch } from 'react-icons/vsc';
+
+// styles for react icon
+const iconStyles = { 
+	fill: '#fff', 
+	position: 'absolute', 
+	background: '#DA4453',
+	height:'30px',
+	width:'30px',
+	padding: '6px',
+	left: '-20px',
+	top: '-22px',
+	border: '1px solid #DA4453',
+	borderRadius:"100% 0 0 100%",
+}
 
 const SearchBox = ({ searchChange }) => {
 	return (
 		<div className='search'>
-			<input onChange={ searchChange } className='search__input tc' type='search' placeholder='search for property type' />
+			<label htmlFor='search' aria-label="Search:">
+				<VscSearch style={iconStyles} size="1.5em" />
+			</label>
+			<input id='search' onChange={ searchChange } className='search__input' type='search' placeholder='search for property type' />
 		</div>
 	);
 }
